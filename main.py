41d07075bfadd753600a604a711fa09d71e56237
Gabriel Castro <ghscastro@hotmail.com>
2022-10-18 21:27:17 -0300

    Importação da lib BeautifulSoup da bs4 | Criação da variável soup, que obtem o que a resposta da URL sendo passada para HTML, para que o soup entenda a página como objetos

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
 res = requests.get("https://webscraper.io/test-sites/e-commerce/allinone")
 res.encoding = "utf-8"
 
-print(res)
+soup = BeautifulSoup(res.text, 'html.parser')
+
+print(soup)
